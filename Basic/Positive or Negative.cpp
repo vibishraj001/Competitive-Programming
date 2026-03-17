3287c691a137018017fffe00221e8f6a4ca3626d
Vibishraj <128944659+vibishraj001@users.noreply.github.com>
2026-03-18 04:24:35 +0530

    Add C++ program to check number sign
    Implement a program to check if a number is positive, negative, or zero.

---
+++ Basic/Positive or Negative.cpp
@@ -0,0 +1,27 @@
+// C++ Program to check whether a number is positive or negative
+// Check if a Number is Positive or Negative in C++
+// Given an integer input, The objective is to write a code to Check if a Number is Positive or Negative in C++ Language.
+
+// For Instance,
+// Input : num = 10
+// Output :The number is Positive
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    int n ;
+
+    cin>>n;
+
+    if(n>0)
+        cout<<"Positive";
+    else if (n<0)
+        cout<<"Negative";
+    else
+        cout<<"Zero";
+
+    return 0;
+}
